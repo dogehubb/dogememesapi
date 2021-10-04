88f194134e0f5b54e7f19106aa3ca34626a7c694
dogehubb <91796458+dogehubb@users.noreply.github.com>
2021-10-04 09:54:05 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,34 +1,69 @@
 const DUMMY_DATA = [
   {
     id: 1,
+    source:"subham",
     title: "ipl teams ft doges",
     url: "https://drive.google.com/uc?export=view&id=1F50NLtgr4a6O56x7zvEd8_bL_EuWcDoj",
   },
   {
     id: 2,
+    source:"nikhil",
     title: "cooking goes wrong",
     url: "https://drive.google.com/uc?export=view&id=1WXTX_lwm33gQLBN-zLvJCoHId3NuVDsf",
   },
   {
     id: 3,
-
+    source:"suman",
     title: "give dogecoin to all",
     url: "https://drive.google.com/uc?export=view&id=1OCJgLjqJdD5PtKKpC3oA3Yn8GuEmwmnM",
   },
   {
     id: 4,
+    source:"abhinav",
     title: "evoloution of phones",
     url: "https://drive.google.com/uc?export=view&id=1oMovMZcvdb2iXULq9oTsYwtTUzLvNc8j",
   },
   {
     id: 5,
-    title: "elon with doge",
-    url: "https://drive.google.com/uc?export=view&id=1WxfrZGxI8sIlFK8TB30miaaYpPC-WLYk",
+    source:"doge cheems",
+    title: "no eating in class ft backbenchers",
+    url: "https://drive.google.com/uc?export=view&id=1wnmVAiMZflivBfZ6QhmbCG3F5BEzQAvE",
   },
-    {
+  {
     id: 6,
-    title: "elon with doge",
-    url: "https://drive.google.com/uc?export=view&id=1WxfrZGxI8sIlFK8TB30miaaYpPC-WLYk",
+    source:"doge cheems",
+    title: "doge gulambi vs spelender",
+    url: "https://drive.google.com/uc?export=view&id=1elzb3iw0YbXtsFYoot1lBGIuTWA8vSal",
+  },
+  {
+    id: 7,
+    source:"doge cheems",
+    title: "doge pav bhaji riya",
+    url: "https://drive.google.com/uc?export=view&id=1n-Gz371b2mk-nOy4f0AbnXMNHWlb3k80",
+  },
+  {
+    id: 8,
+    source:"doge cheems",
+    title: "rcb never gets trophy",
+    url: "https://drive.google.com/uc?export=view&id=1lpefiF2B1AqqzOuL_d6bohHJag0-0gKX",
+  },
+  {
+    id: 9,
+    source:"doge cheems",
+    title: "girls have no brain",
+    url: "https://drive.google.com/uc?export=view&id=1QW00arJn0yQ0_DOB2b5LkzK_ECKF1TqK",
+  },
+  {
+    id: 10,
+    source:"doge cheems",
+    title: "reels moms",
+    url: "https://drive.google.com/uc?export=view&id=1aS0mIw1HbZo-B6oBOdaFRKUg1xiCucHy",
+  },
+  {
+    id: 11,
+    source:"doge cheems",
+    title: "giving hotdpot to friends",
+    url: "https://drive.google.com/uc?export=view&id=1T_1eB1g4NO3Proc73uG2fgt0kHMobmgd",
   },
 ];
 module.exports = DUMMY_DATA;
